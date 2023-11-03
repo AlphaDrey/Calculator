2313da71a392a91e0e77e8bdbf73f3e469de9e89
AlphaDrey <mtdarex1@gmail.com>
2023-11-03 17:36:41 -0400

    First Commit

---
+++ calculator.py
@@ -0,0 +1,16 @@
+num1 = int(input("Enter a number "))
+operator = input("Enter an operator ")
+num2 = int(input("enter anothe numner "))
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)   
+elif operator == "*":
+    print(num1 - num2)
+elif operator == "/":
+    print(num1 - num2)   
+elif operator == "%":
+    print(num1 - num2)   
+else:
+    print("invalid operator")
